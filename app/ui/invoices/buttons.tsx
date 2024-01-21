17e812b3827307c4ab3794804e2b141b8118a828
kotaro <leoleotmn@gmail.com>
2024-01-21 19:11:14 +0900

    12章

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -23,13 +23,3 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-export function DeleteInvoice({ id }: { id: string }) {
-  return (
-    <>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Delete</span>
-        <TrashIcon className="w-5" />
-      </button>
-    </>
-  );
-}
